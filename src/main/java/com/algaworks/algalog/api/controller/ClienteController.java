46af9855f1efd052f68d0f2231201987f753143e
Moises Alves Costa <moi.ac@hotmail.com>
2022-01-15 13:45:14 -0300

    criado repositorio cliente JPA

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -8,28 +8,28 @@ import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
+
+import lombok.AllArgsConstructor;
 
 
 
- 
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 	
-	
-	@PersistenceContext // injeção
-	private EntityManager manager;
+	//@Autowired injetar com anotação ou com lonbok
+	private ClienteRepository clienteRepository;
 	
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-	
-			return manager.createQuery("from Cliente",Cliente.class).getResultList();
-		
+		return clienteRepository.findByNomeContaining("Moi");
 	}
 	
 	
