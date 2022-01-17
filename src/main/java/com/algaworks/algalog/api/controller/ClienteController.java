d06b555d6d2f757b19b0c064d46011726234c939
Moises Alves Costa <moi.ac@hotmail.com>
2022-01-17 20:33:13 -0300

    feat: Domain services e regras de negocio

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -26,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
+import com.algaworks.algalog.domain.service.CatalogoClienteService;
 
 import lombok.AllArgsConstructor;
 
@@ -36,9 +37,8 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/clientes")
 public class ClienteController {
 	
-	//@Autowired injetar com anotação ou com lonbok
 	private ClienteRepository clienteRepository;
-	
+	private CatalogoClienteService catalogoClienteService;
 	
 	@GetMapping
 	public List<Cliente> listar() {
@@ -60,7 +60,7 @@ public class ClienteController {
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente adicionar(@Valid @RequestBody Cliente  cliente) {
-		return clienteRepository.save(cliente);
+		return catalogoClienteService.salvar(cliente);
 	}
 	
 	@PutMapping( "/{clienteId}")
@@ -71,7 +71,7 @@ public class ClienteController {
 		}
 		
 		cliente.setId(clienteId);
-		cliente = clienteRepository.save(cliente);
+		cliente = catalogoClienteService.salvar(cliente);
 		
 		return ResponseEntity.ok(cliente);
 	}
@@ -85,7 +85,7 @@ public class ClienteController {
 		return ResponseEntity.notFound().build();
 	}
 	
-	clienteRepository.deleteById(clienteId);
+	catalogoClienteService.excluir(clienteId);
 	return ResponseEntity.noContent().build();
 	}
 }
