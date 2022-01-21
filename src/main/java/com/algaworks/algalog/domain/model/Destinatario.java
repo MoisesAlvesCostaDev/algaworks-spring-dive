4abeb1feb067a28091002619959deb0e3b3ee7b5
Moises Alves Costa <moi.ac@hotmail.com>
2022-01-20 21:08:20 -0300

    feat: Isolando Domain model

--- src/main/java/com/algaworks/algalog/domain/model/Destinatario.java
+++ src/main/java/com/algaworks/algalog/domain/model/Destinatario.java
@@ -16,14 +16,18 @@ public class Destinatario {
 	@Column(name="destinatario_nome")
 	private String nome;
 	@NotBlank
+	
 	@Column(name="destinatario_logradouro")
 	private String logradouro;
 	@NotBlank
+	
 	@Column(name="destinatario_numero")
 	private String numero;
+	
 	@NotBlank
 	@Column(name="destinatario_complemento")
 	private String complemento;
+	
 	@NotBlank
 	@Column(name="destinatario_bairro")
 	private String bairro;
